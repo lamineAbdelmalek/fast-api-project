e9050d1c1a7ad2fe9a0569021ba5b0961c533c57
lamine <lamine.abdelmalek@gmail.com>
2025-02-02 18:07:26 +0100

    client portfolio and orders table

--- init_db.sql
+++ init_db.sql
@@ -5,6 +5,21 @@ CREATE TABLE IF NOT EXISTS company_credit_scores (
     SCORE_TYPE CHAR(1) CHECK (SCORE_TYPE IN ('X', 'Y'))
 );
 
+CREATE TABLE IF NOT EXISTS client_portfolio (
+    COMPANY_ID VARCHAR(12) NOT NULL,
+    VALIDITY_START_DATE TIMESTAMP NOT NULL,
+    VALIDITY_END_DATE TIMESTAMP,
+    PORTFOLIO_ENTRY_ID VARCHAR(256) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS client_orders (
+    ORDER_DATE TIMESTAMP NOT NULL,
+    COMPANY_ID VARCHAR(12) NOT NULL,
+    ORDER_TYPE VARCHAR(12) NOT NULL CHECK (
+        ORDER_TYPE IN ('scores', 'claims', 'score_updates', 'claim_updates')
+    )
+);
+
 -- Populate the table only if it's empty
 DO
 $$
