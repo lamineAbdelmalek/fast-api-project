e567433a2df0f5b976e2685639435fab825cae70
lamine <lamine.abdelmalek@gmail.com>
2025-02-02 17:27:42 +0100

    add abstract class and refactor code

--- awesome_api/utils/postgres_utils.py
+++ awesome_api/utils/postgres_utils.py
@@ -1,9 +1,12 @@
 import os
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Optional
 
 import pandas as pd
 from dotenv import load_dotenv
 from pandas import DataFrame
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 from awesome_api.constants import (
     ENV_VAR_POSTGRES_DATABASE_URL,
@@ -13,37 +16,61 @@ from awesome_api.constants import (
 )
 
 
-def run_select_query(query: str) -> DataFrame:
-    # Get database url
-    database_url = get_db_url()
+class BaseDataSource(ABC):
+    """Abstract class for data source connection and querying."""
 
-    # Create a database engine
-    engine = create_engine(database_url)
+    @abstractmethod
+    def get_db_url(self) -> str:
+        """Establish a connection to the data source."""
+        pass
 
-    # Execute the query and load results into a Pandas DataFrame
-    df = pd.read_sql(query, engine)
-
-    # Close the connection
-    engine.dispose()
-
-    return df
+    @abstractmethod
+    def run_select_query(self, query: str) -> DataFrame:
+        """Run a SELECT query and return the results."""
+        pass
 
 
-def get_db_url() -> str:
-    if ENV_VAR_POSTGRES_DATABASE_URL in os.environ:
-        # docker mode
-        db_url = os.environ[ENV_VAR_POSTGRES_DATABASE_URL]
-    else:
-        # local mode
-        load_dotenv(".env")
-        user = os.environ[ENV_VAR_POSTGRES_USER]
-        password = os.environ[ENV_VAR_POSTGRES_PASSWORD]
-        db_name = os.environ[ENV_VAR_POSTGRES_DB]
-        host = "localhost"
-        db_url = f"postgresql://{user}:{password}@{host}:5432/{db_name}"
-    return db_url
+class PostgresDataSource(BaseDataSource):
+    """PostgreSQL implementation of BaseDataSource."""
 
+    def __init__(self):
+        """Class init"""
 
-def run_sql_example() -> None:
-    df = run_select_query(query="SELECT * FROM company_credit_scores LIMIT 10;")
-    print(df)
+    def run_select_query(
+        self, query: str, params: Optional[Dict[str, Any]] = None
+    ) -> DataFrame:
+        # Get database url
+        database_url = self.get_db_url()
+
+        # Create a database engine
+        engine = create_engine(database_url)
+
+        # Execute the query and load results into a Pandas DataFrame
+        if params:
+            df = pd.read_sql(text(query), engine, params=params)
+        else:
+            df = pd.read_sql(text(query), engine)
+        # Close the connection
+        engine.dispose()
+
+        return df
+
+    def get_db_url(self) -> str:
+        if ENV_VAR_POSTGRES_DATABASE_URL in os.environ:
+            # docker mode
+            db_url = os.environ[ENV_VAR_POSTGRES_DATABASE_URL]
+        else:
+            # local mode
+            load_dotenv(".env")
+            user = os.environ[ENV_VAR_POSTGRES_USER]
+            password = os.environ[ENV_VAR_POSTGRES_PASSWORD]
+            db_name = os.environ[ENV_VAR_POSTGRES_DB]
+            host = "localhost"
+            db_url = f"postgresql://{user}:{password}@{host}:5432/{db_name}"
+        return db_url
+
+    def run_sql_example(self) -> None:
+        df = self.run_select_query(
+            query="SELECT * FROM company_credit_scores LIMIT 10;"
+        )
+        print(df)
